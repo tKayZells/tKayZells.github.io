f6208789407e3c4180ddc8f6cc8253ba16512c6d
vicec <dev.miguellucena@gmail.com>
2021-10-24 16:10:34 +0200

    Update site

---
+++ _next/static/lKyl8AGD48VLtfTJZa8Lv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/166ce19acc06d3afb0e4.css","static/chunks/pages/index-ff9a7e0cd460f14c4b14.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
